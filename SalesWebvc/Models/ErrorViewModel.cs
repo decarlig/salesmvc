eff047148ae57cb0660da0d607095337e47ebed8
Gian <giandecarli@hotmail.com>
2020-05-04 00:47:54 -0300

    Project create

---
+++ SalesWebvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesWebvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
