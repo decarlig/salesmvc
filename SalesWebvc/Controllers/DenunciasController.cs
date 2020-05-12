5195ba5ee7b29eece8ebcd017b19e64966ce47bf
Gian <giandecarli@hotmail.com>
2020-05-12 02:30:40 -0300

    CRUD Scaffolding

---
+++ SalesWebvc/Controllers/DenunciasController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SalesWebvc.Data;
+using SalesWebvc.Models;
+
+namespace SalesWebvc.Controllers
+{
+    public class DenunciasController : Controller
+    {
+        private readonly SalesWebvcContext _context;
+
+        public DenunciasController(SalesWebvcContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Denuncias
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Denuncia.ToListAsync());
+        }
+
+        // GET: Denuncias/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var denuncia = await _context.Denuncia
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (denuncia == null)
+            {
+                return NotFound();
+            }
+
+            return View(denuncia);
+        }
+
+        // GET: Denuncias/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Denuncias/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Logradouro,Numero,Complemento,Municipio,Estado,Zona,Tipo,Aream3,Responsavel")] Denuncia denuncia)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(denuncia);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(denuncia);
+        }
+
+        // GET: Denuncias/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var denuncia = await _context.Denuncia.FindAsync(id);
+            if (denuncia == null)
+            {
+                return NotFound();
+            }
+            return View(denuncia);
+        }
+
+        // POST: Denuncias/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Logradouro,Numero,Complemento,Municipio,Estado,Zona,Tipo,Aream3,Responsavel")] Denuncia denuncia)
+        {
+            if (id != denuncia.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(denuncia);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DenunciaExists(denuncia.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(denuncia);
+        }
+
+        // GET: Denuncias/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var denuncia = await _context.Denuncia
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (denuncia == null)
+            {
+                return NotFound();
+            }
+
+            return View(denuncia);
+        }
+
+        // POST: Denuncias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var denuncia = await _context.Denuncia.FindAsync(id);
+            _context.Denuncia.Remove(denuncia);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DenunciaExists(int id)
+        {
+            return _context.Denuncia.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ SalesWebvc/Data/SalesWebvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebvc.Models;
+
+namespace SalesWebvc.Data
+{
+    public class SalesWebvcContext : DbContext
+    {
+        public SalesWebvcContext (DbContextOptions<SalesWebvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebvc.Models.Denuncia> Denuncia { get; set; }
+    }
+}
